5a4a02985913675bf466c1b8514e6a98e6a0bed7
EL CODER <sdetsoloman@gmail.com>
2022-09-25 19:11:25 -0500

    day 5 and day 6 are done

--- just_practicing.py
+++ just_practicing.py
@@ -1,3 +1,20 @@
-nums = [1, 2, 3, 4, 5, 6]
+def diamond(n):
+    if n % 2 != 0 and n > 0:
+        my_diamond = ""
+        for num in range(1, n + 1):
+            if num % 2 != 0:
+                my_diamond += "*" * num
+                my_diamond += "\n"
+        for num in range(n - 1, 0, -1):
+            if num % 2 != 0:
+                my_diamond += "*" * num
+                my_diamond += "\n"
+        return my_diamond
+    return None
 
-print({num:("X" if num % 2 == 0 else "O") for num in nums})
+
+print(diamond(5))
+
+for num in range(1, 8 + 1):
+    if num % 2 != 0:
+        print(" " * (8 - num) + "*" * num + "\n")
